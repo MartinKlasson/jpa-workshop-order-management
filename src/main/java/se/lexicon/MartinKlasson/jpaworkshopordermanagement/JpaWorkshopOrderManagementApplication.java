0859c222db8949ba6bc4547c1a433f442266bc86
MartinKlasson <tillmartin2000@yahoo.se>
2020-08-13 10:41:50 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/MartinKlasson/jpaworkshopordermanagement/JpaWorkshopOrderManagementApplication.java
@@ -0,0 +1,14 @@
+package se.lexicon.MartinKlasson.jpaworkshopordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshopOrderManagementApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(JpaWorkshopOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/MartinKlasson/jpaworkshopordermanagement/JpaWorkshopOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.MartinKlasson.jpaworkshopordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
